cbee4570a9df3e0c33a8fbadb85c315db27fc980
Tissa Khosla <tissa.music@gmail.com>
2019-12-15 16:25:09 -0500

    Updates

---
+++ precache-manifest.e56a568b622a3baa991a4588fb607692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ee3e3bbcd6ee99296401a60c81b2d5",
+    "url": "/test_deploy/index.html"
+  },
+  {
+    "revision": "a1de807aa7eccf29e7b1",
+    "url": "/test_deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf0e506fc66d7ddd3761",
+    "url": "/test_deploy/static/js/2.ed1d231c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test_deploy/static/js/2.ed1d231c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1de807aa7eccf29e7b1",
+    "url": "/test_deploy/static/js/main.91af82fc.chunk.js"
+  },
+  {
+    "revision": "9d52414109f793aef140",
+    "url": "/test_deploy/static/js/runtime-main.a5fe00f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
